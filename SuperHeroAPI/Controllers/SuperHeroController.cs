3d0e8d48c33d28ce0d087bfd9e33c4cdb9e37ef9
Oluwatobiloba Agbeja <tobiagbeja4@gmail.com>
2023-01-30 16:51:27 +0100

    initial commit

---
+++ SuperHeroAPI/Controllers/SuperHeroController.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using System.Xml.Linq;
+using SuperHeroAPI.Data;
+
+namespace SuperHeroAPI.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class SuperHeroController: ControllerBase
+	{
+
+		private static List<SuperHero> heroes = new List<SuperHero>
+		{
+            new SuperHero{
+                Id= 1,
+                Name="Spider Man",
+                FirstName="Peter",
+                LastName="Parker",
+                Place="New York City"
+            },
+            new SuperHero{
+                Id= 2,
+                Name="Iron Man",
+                FirstName="Tony",
+                LastName="Stark",
+                Place="Long Island"
+            }
+        };
+        private readonly DataContext _context;
+
+        public SuperHeroController(DataContext context)
+        {
+            this._context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<SuperHero>>> Get()
+        {
+            return Ok(this._context.SuperHeroes.ToList());
+            //return Ok(heroes);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<List<SuperHero>>> Get(int id)
+        {
+            var hero = heroes.Find(h => h.Id == id);
+            if (hero == null)
+                return BadRequest("Hero not found!");
+
+            return Ok(hero);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<List<SuperHero>>> AddHero(SuperHero hero)
+        {
+            //heroes.Add(hero);
+            this._context.SuperHeroes.Add(hero);
+            this._context.SaveChanges();
+
+            return Ok(this._context.SuperHeroes.ToList());
+            //return Ok(heroes);
+        }
+
+        [HttpPut]
+        public async Task<ActionResult<List<SuperHero>>> UpdateHero(SuperHero request)
+        {
+            var hero = heroes.Find(h => h.Id == request.Id);
+            if (hero == null)
+                return BadRequest("Hero not found!");
+            hero.Name = request.Name;
+            hero.FirstName = request.FirstName;
+            hero.LastName = request.LastName;
+            hero.Place = request.Place;
+
+            return Ok(hero);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<List<SuperHero>>> Delete(int id)
+        {
+            var hero = heroes.Find(h => h.Id == id);
+            if (hero == null)
+                return BadRequest("Hero not found!");
+            heroes.Remove(hero);
+
+            return Ok(heroes);
+        }
+    }
+}
+
---
+++ SuperHeroAPI/Data/DataContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace SuperHeroAPI.Data
+{
+	public class DataContext: DbContext
+	{
+        public DataContext(DbContextOptions<DataContext> options): base(options)
+        {
+            //SuperHeroes = superHeroes;
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.UseSerialColumns();
+        }
+        public DbSet<SuperHero> SuperHeroes { get; set; }
+	}
+}
+
